b715db53fb6ad7cfb51d9ff55deaeecdee7102d3
nad-15 <123393892+nad-15@users.noreply.github.com>
2025-02-09 23:05:03 +0800

    optimize code for better readability and scalability

--- script.js
+++ script.js
@@ -5,29 +5,58 @@ const createGridButton = document.getElementById(`create-grid-button`);
 const gridContainer = document.getElementById(`grid-container`);
 // gridContainer.style.backgroundColor = `red`;
 
-createGridButton.addEventListener(`click`, createGrid);
+createGrid(16);
 
-function createGrid() {
-
-    if (numOfSquareInput.value < 1 || numOfSquareInput.value > 100) {
-        alert(`Enter valid number: Min: 1, Max: 100`);
-        return;
+createGridButton.addEventListener('click', function () {
+    const squareNum = parseInt(numOfSquareInput.value);
+    // Only create grid if squareNum is a valid number
+    if (squareNum >= 1 && squareNum <= 100) {
+        createGrid(squareNum);
+    } else {
+        alert('Enter valid number: Min: 1, Max: 100');
     }
+});
 
 
-    gridContainer.innerHTML = ''; 
+const colors = [
+    "Red", "Blue", "Green", "Yellow", "Orange",
+    "Purple", "Cyan", "Magenta", "Pink", "Brown",
+    "Gray", "Teal", "Lime"
+];
 
-    for(let i=1; i<=parseInt(numOfSquareInput.value); i++) {
-        console.log(`creating grid row ${i}`);
+
+function getRandomColor() {
+    const randomColor = Math.floor((Math.random() * 13));
+    console.log(randomColor);
+    return colors[randomColor];
+}
+
+function createGrid(squareNum) {
+
+    gridContainer.innerHTML = '';
+
+    for (let i = 1; i <=squareNum; i++) {
+
         const rowGrid = document.createElement(`div`);
         rowGrid.classList.add(`row-grid`);
-        for(let j=1; j<=parseInt(numOfSquareInput.value); j++) {
-            console.log(`creating grid row ${j}`);
+        for (let j = 1; j <=squareNum; j++) {
+
             const squares = document.createElement(`div`);
-            // squares.textContent = 'i';
+
             squares.classList.add(`squares`);
+            squares.addEventListener(`mouseenter`, ()=>{
+                squares.style.backgroundColor = getRandomColor();
+            });
+
+
+            squares.addEventListener(`mouseout`, ()=>{
+                squares.style.backgroundColor = ``;
+            });
+
+
             rowGrid.appendChild(squares);
         }
         gridContainer.appendChild(rowGrid);
     }
 };
+
